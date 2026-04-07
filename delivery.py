218b47c91c5d17c6c6f98923b136f68bd1fedd3a
Kuppam Dheepesh Gupta <141455329+Dheepesh007@users.noreply.github.com>
2026-04-07 16:24:11 +0530

    Add delivery time check functionality

---
+++ delivery.py
@@ -0,0 +1,11 @@
+# Delivery Time Check
+
+time_taken = 95   # seconds
+limit = 100       # permissible time
+
+if time_taken <= limit:
+    print("Package Delivered")
+else:
+    print("Delivery Failed")
+
+print("Output: Delivered")
